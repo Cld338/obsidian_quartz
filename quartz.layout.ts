29de3cc06013ff8ac2062557c333294ae81753d7
cld338 <jihuno291@gmail.com>
2024-03-20 22:08:38 +0900

    modified:   quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Explorer(),
+    Component.MobileOnly(Component.Explorer()),
     Component.Backlinks(),
     Component.DesktopOnly(Component.Darkmode()),
   ]
